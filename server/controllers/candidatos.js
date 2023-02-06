bf8e4016d720521208407d12b0509848816f81f6
joaonetocastro <j.neto111@gmail.com>
2023-02-06 09:12:54 -0300

    feat: filtar por cargo

--- server/controllers/candidatos.js
+++ server/controllers/candidatos.js
@@ -1,6 +1,12 @@
 const {db} = require('../db')
 
 const getCandidatos = (req, res) => {
+  let where = 'WHERE'
+  console.log(req.query)
+  if(req.query.cargo) {
+    where += ` c.cargo = ${req.query.cargo}`
+  }
+
   const query = `
     SELECT 
     c.*, tc.nome as nome_tipo, cg.nome as nome_cargo, count(v.candidato) as quantidade_de_votos
@@ -8,8 +14,10 @@ const getCandidatos = (req, res) => {
     INNER JOIN tipo_candidato tc on tc.id = c.tipo
     INNER JOIN cargo cg on cg.id = c.cargo
     LEFT JOIN votacao v on v.candidato = c.id
+    ${where !== 'WHERE' ? where : ''}
     GROUP BY c.id
   `;
+
 
   return new Promise(function (resolve, reject) {
     db.all(query, async (err, rows) => {
