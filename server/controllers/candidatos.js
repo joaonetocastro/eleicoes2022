72f92185eb1d71549f417c0822bed80b1dc07ae7
joaonetocastro <j.neto111@gmail.com>
2023-02-06 08:57:32 -0300

    feat: use db from other file

--- server/controllers/candidatos.js
+++ server/controllers/candidatos.js
@@ -1,26 +1,20 @@
-const sqlite = require('sqlite3');
-const db = new sqlite.Database('./database/eleicoes2022-pi.db');
+const {db} = require('../db')
 
 const getCandidatos = (req, res) => {
-  const query = `SELECT c.*, tc.nome as nome_tipo FROM candidato c INNER JOIN tipo_candidato tc on tc.id = c.tipo`;
+  const query = `
+    SELECT 
+    c.*, tc.nome as nome_tipo, cg.nome as nome_cargo 
+    FROM candidato c 
+    INNER JOIN tipo_candidato tc on tc.id = c.tipo
+    INNER JOIN cargo cg on cg.id = c.cargo
+  `;
 
   return new Promise(function (resolve, reject) {
-    let response = [];
     db.all(query, async (err, rows) => {
       if (err) {
         res.status(500).send({message: err.message});
         return
       }
-
-      // response = rows.map((cand) => {
-      //   return {
-      //     name: cand.cand_nome,
-      //     role: cand.cargo_nome,
-      //     votes: cand.cand_votos,
-      //     status: cand.cand_status == 1 ? "elected" : "not elected",
-      //   }
-      // });
-
       res.send(rows);
     });
   });
