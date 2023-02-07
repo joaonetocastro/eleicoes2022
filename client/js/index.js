e94c27a4061467bdaa066f7b635f9c6bc07f1cd7
joaonetocastro <j.neto111@gmail.com>
2023-02-07 20:44:02 -0300

    feat: get candidates

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,27 @@
+function getCandidates(onSuccess, onError) {
+  const xhr = new XMLHttpRequest();
+  const url = "/api/candidates";
+
+  xhr.addEventListener('loadend', function() {
+    const responseBody = JSON.parse(this.responseText);
+    if(this.status !== 200) {
+      onError(responseBody)
+      return
+    }
+    onSuccess(responseBody)
+  })
+
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+const api = {
+  getCandidates
+}
+
+
 window.addEventListener("load", (event) => {
   console.log("Carregou!");
 });
+
+api.getCandidates(console.log, console.error)
