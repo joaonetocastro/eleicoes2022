36fbc9990bdc6b9a1635d2a1a4365863cb4b62ce
joaonetocastro <j.neto111@gmail.com>
2023-02-07 21:26:16 -0300

    feat: list candidates

--- client/js/index.js
+++ client/js/index.js
@@ -20,7 +20,7 @@ function makeEndpointFetcher(props){
   return endpointFetcher
 }
 
-const api = {
+var api = {
   getCandidates: makeEndpointFetcher({ url: '/api/candidates'}),
   getCandidatesByCity: makeEndpointFetcher({url: '/api/cities'}),
   getCandidatesByRole: makeEndpointFetcher({url: '/api/roles'}),
@@ -28,9 +28,56 @@ const api = {
   getCities: makeEndpointFetcher({url: '/api/list/cities'}),
 }
 
+function getSelectedCandidates(){
+  const checked = document.querySelectorAll('[name=selected_candidates]:checked')
+  const candidates = []
+  for(const element of checked) {
+    candidates.push(({id: element.value, name: element.dataset.name}))
+  }
+  return candidates
+}
+
+var applicationState = {
+  getSelectedCandidates
+}
+
+function renderCandidates(candidates) {
+  const container = document.querySelector('#candidate-container')
+  container.replaceChildren()
+  for(const candidate of candidates) {
+    const element = document.createElement('div')
+    element.innerHTML = `
+      <input type="checkbox" name="selected_candidates" value="${candidate.cand_id}" data-name="${candidate.cand_nome}">${candidate.cand_nome}</input>
+    `
+    
+    container.append(element)
+  }
+}
+
+// TODO:
+// 1. O sistema deverá permitir que sejam consultados os resultados das seguintes formas:
+
+// 1.1. Por candidato
+// ∘Deve ser possível selecionar o candidato por seu nome a partir de uma caixa de edição e/ou uma lista.
+// ∘No resultado apresentado deve constar nome, cargo, votação e status (eleito ou não eleito).
+
+// 1.2. Por cargo
+// ∘Ao se selecionar o cargo desejado a partir de uma lista, deve-se apresentar uma relação de todos os
+// candidatos contendo (i) nome, (ii) quantidade de votos recebidos e (iii) status (eleito ou não eleito).
+// ∘No resultado apresentado deve constar nome, cargo, votação e status (eleito ou não eleito).
+
+// 1.3. Por município
+// ∘Ao se selecionar o município, deve ser exibida a votação de todos os candidatos que receberam votação
+// naquele município.
+// ∘No resultado apresentado deve constar nome, cargo, votação e status (eleito ou não eleito).
+// ∘Ao final do resultado deve ser exibida a totalização dos votos.
+
+// 1.4. Resultado Geral
+// ∘Exibir o resultado geral da votação no Estado para todos os cargos eletivos.
+// ∘Deve ser possível apresentar a votação global de todos os candidatos ou apenas dos candidatos eleitos.
+// ∘A alternância entre “eleitos” e “não eleitos” deve ser implementada utilizando-se a técnica AJAX.
+
 
 window.addEventListener("load", (event) => {
-  console.log("Carregou!");
+  api.getCandidates(renderCandidates)
 });
-
-api.getCandidatesByRole(console.log, console.error, {query: {search: 'presidente'}})
